79e31d8bfd37f67aa16a9b7ce90a74a42679cf0a
Marvel-Data <marvel.bolaji@gmail.com>
2023-04-10 20:22:17 +0000

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a given char is digit.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
