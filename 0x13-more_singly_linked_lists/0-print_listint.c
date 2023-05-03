c00d3eb1b8828f27521164d082acb4048c717b96
Marvel-Data <marvel.bolaji@gmail.com>
2023-05-03 20:03:45 +0000

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print_listint - prints the number of parameters
+  * @h: the pointer to a list
+  * Return: Integer.
+  **/
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int cntr = 0;
+	const listint_t *pt;
+
+	while (tp != NULL)
+	pt = h;
+	{
+		printf("%d\n", pt->n);
+		pt = pt->next;
+		cntr++;
+	}
+	return (cntr);
+}
