ff808b1046c9c9729b33ce4f1a2a74148c9529ba
Marvel-Data <marvelkode@Marvel.localdomain>
2023-05-25 13:25:58 +0100

    copy_file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+  * main - Entry point to the function
+  * @argc: The count
+  * @argv: The vector
+  *
+  * Return: integer
+  */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_(argv[1], argv[2]);
+	exit(0);
+}
+
+/**
+  * copy_ - copy file
+  * @src: first argument
+  * @dest: second argument
+  *
+  * Return: nothing
+  */
+void copy_(const char *src, const char *dest)
+{
+	int ofd, tfd, readed;
+	char buff[1024];
+
+	ofd = open(src, O_RDONLY);
+	if (!src || ofd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	tfd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((readed = read(ofd, buff, 1024)) > 0)
+	{
+		if (write(tfd, buff, readed) != readed || tfd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			exit(99);
+		}
+	}
+
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	if (close(ofd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ofd);
+		exit(100);
+	}
+
+	if (close(tfd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfd);
+		exit(100);
+	}
+}
