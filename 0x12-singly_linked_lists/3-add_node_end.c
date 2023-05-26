6c425e952b16739b0fca180a6d5b1641947c77ce
Marvel-Data <marvelkode@Marvel.localdomain>
2023-05-26 22:20:42 +0100

    add_node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,9 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node to the end
- * @head: first argument
- * @str: data
- *
- * Return: List_t
- */
-list_t *add_node_end(list_t **head, const char *str)
-{
-	list_t *new, *test;
-
-	if (str == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(list_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
-	new->len = strlen(str);
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-
-	else
-	{
-		test = *head;
-
-		while (test->next != NULL)
-			test = test->next;
-		test->next = new;
-	}
-
-	return (new);
-}
-#include "lists.h"
-
-/**
  * add_node_end - adds a new node to the endi
  * @head: first argument
- * @str: string
+ * @str: data
  *
  * Return: List_t
  */
